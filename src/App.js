e8f44ea2eaa331c693d000f0499fa6c308d3da8e
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-01 21:52:57 +0200

    Patch[0.0.1]
    Champions fetched from Riot api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-export default function App(){
+import { data } from "autoprefixer";
+import logo from "./logo.png";
+export default function App() {
 
-    return <h1 className="bg-red-500">League of api</h1>
+  fetch("http://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion.json").then(res=>res.json()).then(data=>console.log(data.data))
+  return (
+    <>
+      <div className="relative w-fit mx-auto">
+        <img src={logo} width="500" className="mx-auto" />
+        <div className="absolute bg-red-500 h-5 w- bottom-32"></div>
+      </div>
+    </>
+  );
 }
