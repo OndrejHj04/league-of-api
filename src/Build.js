caf82146da17b9f9c2b4e4b3bf8677fa0b0edfd2
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-02 21:43:05 +0200

    Patch[0.1]
    Form layout

---
+++ src/Build.js
@@ -0,0 +1,4 @@
+export default function Build(){
+
+    return <h1>css</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import "./style.css"
-const container = document.getElementById('root');
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Build from "./Build";
+import "./style.css";
+const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
