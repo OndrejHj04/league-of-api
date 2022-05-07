e287b7c162737b3c9f11f9bd843410d42c99ad69
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-05-07 13:18:17 +0200

    Pre-patch [1.0]

--- src/Build.js
+++ src/Build.js
@@ -1,4 +1,5 @@
-export default function Build({ champion, mainTree, items }) {
+export default function Build({ champion, mainTree, items, ss, guardians }) {
+    console.log(guardians)
   return (
     <div className="max-w-lg mx-auto">
       <>
@@ -6,14 +7,17 @@ export default function Build({ champion, mainTree, items }) {
           <img src={`http://ddragon.leagueoflegends.com/cdn/12.8.1/img/champion/${champion.id}.png`} alt="" width="128" />
           <div className="m-auto text-2xl">{champion.title}</div>
         </div>
-        <div className="flex justify-between my-2">
-          <img src={`https://ddragon.canisback.com/img/${mainTree.treeOne}`} alt="" width="112" height="112" className="my-auto" />
-          <img src={`https://ddragon.canisback.com/img/${mainTree.treeTwo}`} alt="" width="112" height="112" className="my-auto"  />
-          <img src={`https://ddragon.canisback.com/img/${mainTree.treeThree}`} alt="" width="112" height="112" className="my-auto" />
-          <img src={`https://ddragon.canisback.com/img/${mainTree.treeFour}`} alt="" width="112" height="112" className="my-auto"/>
-
-          <div></div>
+        <div className="grid grid-cols-4 grid-rows-2  my-2">
+          <img src={`https://ddragon.canisback.com/img/${mainTree.treeOne}`} alt="" width="112" height="112" className="m-auto" />
+          <img src={`https://ddragon.canisback.com/img/${mainTree.treeTwo}`} alt="" width="112" height="112" className="m-auto" />
+          <img src={`https://ddragon.canisback.com/img/${mainTree.treeThree}`} alt="" width="112" height="112" className="m-auto" />
+          <img src={`https://ddragon.canisback.com/img/${mainTree.treeFour}`} alt="" width="112" height="112" className="m-auto" />
+          <img src={`https://ddragon.canisback.com/img/${mainTree.secondFirst}`} alt="" width="112" height="112" className="m-auto"/>
+          <img src={`https://ddragon.canisback.com/img/${mainTree.secondSecond}`} alt="" width="112" height="112" className="m-auto"/>
+          <img src={`http://ddragon.leagueoflegends.com/cdn/12.8.1/img/spell/${ss.full}`} alt="" width="112" height="112" className="m-auto"/>
+          <img src={`http://ddragon.leagueoflegends.com/cdn/12.8.1/img/item/${guardians}`} alt="" width="112" height="112" className="m-auto"/>
         </div>
+
         <div className="flex justify-between">
           <img src={`http://ddragon.leagueoflegends.com/cdn/12.8.1/img/item/${items[0].full}`} alt="" className="my-auto w-1/6 m-1" />
           <img src={`http://ddragon.leagueoflegends.com/cdn/12.8.1/img/item/${items[1].full}`} alt="" className="my-auto w-1/6 m-1" />
--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,7 @@ export default function Rt() {
   const [champions, setChampions] = useState();
   const [items, setItems] = useState();
   const [runes, setRunes] = useState();
+  const [ss, setSs] = useState();
 
   useEffect(() => {
     fetch("http://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion.json")
@@ -28,16 +29,22 @@ export default function Rt() {
     fetch("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/runesReforged.json")
       .then((res) => res.json())
       .then((data) => setRunes(data));
+    fetch("http://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/summoner.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setSs(data);
+      });
   }, []);
 
   const firebaseConfig = {
-    apiKey: "AIzaSyDObszvg5RZasNoTHHg32btNLNbdM3A_Hc",
-    authDomain: "league-of-api.firebaseapp.com",
-    projectId: "league-of-api",
-    storageBucket: "league-of-api.appspot.com",
-    messagingSenderId: "622387037522",
-    appId: "1:622387037522:web:09e6d814a07999c059e68b",
+    apiKey: "AIzaSyBLkpwcdUuiOnl_L76InIsg8wQUKahpA9M",
+    authDomain: "zalozni-6077d.firebaseapp.com",
+    projectId: "zalozni-6077d",
+    storageBucket: "zalozni-6077d.appspot.com",
+    messagingSenderId: "602536796708",
+    appId: "1:602536796708:web:5132a40ce9cd57df5faeb1",
   };
+
   initializeApp(firebaseConfig);
   const db = getFirestore();
   const urlRef = collection(db, "urls");
@@ -48,11 +55,6 @@ export default function Rt() {
 
   const links = (p) => {
     setShowLinks(p);
-  };
-
-  const getRandomChamp = () => {
-    const rand = champions[Object.keys(champions)[Math.floor(Math.random() * Object.keys(champions).length)]];
-    return rand;
   };
 
   const generateSeconds = () => {
@@ -72,37 +74,76 @@ export default function Rt() {
       secondTree = generateSeconds();
     }
 
-    const firstLength = secondTree.slots[Math.floor(Math.random()*3)+1];
-    const secondLength = secondTree.slots[Math.floor(Math.random()*3)+1];
+    let arr = [1, 2, 3];
+    let rand = Math.floor(Math.random() * 3) + 1;
+    arr = arr.filter((i) => i !== rand);
 
-    const secondTreeFirst = firstLength.runes[Math.floor(Math.random()*firstLength.runes.length)]
-    const secondTreeSecond = secondLength.runes[Math.floor(Math.random()*secondLength.runes.length)]
-    // pojisti ze sekundarky budou plny a az pak poslat
+    const firstLength = secondTree.slots[arr[0]];
+    const secondLength = secondTree.slots[arr[1]];
+
+    const secondTreeFirst = firstLength.runes[Math.floor(Math.random() * firstLength.runes.length)];
+    const secondTreeSecond = secondLength.runes[Math.floor(Math.random() * secondLength.runes.length)];
+
     obj = { treeOne: treeOne.icon, treeTwo: treeTwo.icon, treeThree: treeThree.icon, treeFour: treeFour.icon, secondFirst: secondTreeFirst.icon, secondSecond: secondTreeSecond.icon };
     return obj;
   };
 
+  const randomSs = () => {
+    let arr = [ss.data.SummonerFlash, ss.data.SummonerBarrier, ss.data.SummonerDot, ss.data.SummonerExhaust, ss.data.SummonerHeal, ss.data.SummonerHaste];
+    return arr[Math.floor(Math.random() * arr.length)].image;
+  };
+
   const getRandomItems = () => {
     if (items) {
+      let properItems = [];
+      let guardians = []
+      Object.keys(items.data).forEach((i) => {
+        if (items.data[i].maps[12] && items.data[i].gold.purchasable && items.data[i].from && items.data[i].name !== "Broken Stopwatch") {
+          properItems.push(items.data[i]);
+        }else if(items.data[i].name.includes("Guardian")){
+          guardians.push(items.data[i])
+        }
+      });
+      guardians = guardians[Math.floor(Math.random()*guardians.length)].image.full
+
       let arr = [];
-      for (let i = 0; i < 6; i++) {
-        arr.push(items.data[Object.keys(items.data)[Math.floor(Math.random() * Object.keys(items.data).length)]].image);
+      for (let i = 0; i < properItems.length; i++) {
+        arr.push(properItems[Math.floor(Math.random() * properItems.length)].image);
       }
-      return arr;
+      while(arr.length < 6){
+        const rand = properItems[Math.floor(Math.random() * properItems.length)].image
+        if(!arr.includes(rand)){
+          arr.push(rand)
+        }
+      }
+      return [arr, guardians];
     }
+  };
+  const pickRandomChamp = (p) => {
+    let arr = [];
+    while (arr.length < p) {
+      let rand = champions[Object.keys(champions)[Math.floor(Math.random() * Object.keys(champions).length)]];
+      if (!arr.includes(rand)) {
+        arr.push(rand);
+      }
+    }
+    return arr;
   };
 
   useEffect(() => {
     setNewUrls([]);
     if (showLinks) {
-      [...Array(Number(players))].forEach((item) => {
+      let champs = pickRandomChamp(players); //eslint-disable-next-line
+      let res = [...Array(Number(players))].forEach((item, i) => {
         const id = nanoid();
         setDoc(doc(db, "urls", id), {
           url: id,
           time: new Date().getTime(),
-          champion: getRandomChamp(),
+          champion: champs[i],
           mainTree: getMainTree(),
-          items: getRandomItems(),
+          items: getRandomItems()[0],
+          guardians: getRandomItems()[1],
+          ss: randomSs(),
         });
         setNewUrls((oldVal) => {
           return [...oldVal, id];
@@ -120,7 +161,6 @@ export default function Rt() {
       setAllUrls(arr);
     });
   };
-
   const tempData = useRef();
 
   tempData.current = getData;
@@ -145,7 +185,7 @@ export default function Rt() {
       <Routes>
         <Route path="/" element={<App players={players} showLinks={showLinks} getPlayers={getPlayers} links={links} newUrls={newUrls} />}></Route>
         {allUrls.map((item) => (
-          <Route key={item.url} path={item.url} element={<Build champion={item.champion} mainTree={item.mainTree} items={item.items} />} />
+          <Route key={item.url} path={item.url} element={<Build champion={item.champion} mainTree={item.mainTree} items={item.items} ss={item.ss} guardians={item.guardians}/>} />
         ))}
         <Route path="xd" element={<Build />} />
         <Route path="*" element={<ForOuFor />}></Route>
